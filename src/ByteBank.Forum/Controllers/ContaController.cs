bbd41c76a6292d7c2499bfe0d73ae784fa206b23
Guilherme Matheus Costa <costa.guimatheus@gmail.com>
2018-02-07 16:08:13 -0200

    Video 3.3 - correção

--- src/ByteBank.Forum/Controllers/ContaController.cs
+++ src/ByteBank.Forum/Controllers/ContaController.cs
@@ -53,13 +53,14 @@ namespace ByteBank.Forum.Controllers
                 var usuarioJaExiste = usuario != null;
 
                 if (usuarioJaExiste)
-                    return RedirectToAction("Index", "Home");
+                    return View("AguardandoConfirmacao");
 
                 var resultado = await UserManager.CreateAsync(novoUsuario, modelo.Senha);
 
                 if (resultado.Succeeded)
                 {
-                    await EnviarEmailDeConfirmacaoAsync(usuario);
+                    // Enviar o email de confirmação
+                    await EnviarEmailDeConfirmacaoAsync(novoUsuario);
                     return View("AguardandoConfirmacao");
                 }
                 else
@@ -74,25 +75,26 @@ namespace ByteBank.Forum.Controllers
 
         private async Task EnviarEmailDeConfirmacaoAsync(UsuarioAplicacao usuario)
         {
-            var codigoDeConfirmacao = await UserManager.GenerateEmailConfirmationTokenAsync(usuario.Id);
+            var token = await UserManager.GenerateEmailConfirmationTokenAsync(usuario.Id);
 
-            var callbackUrl = Url.Action(
-                "ConfirmacaoEmail",
-                "Conta",
-                new { usuarioId = usuario.Id, codigo = codigoDeConfirmacao },
-                protocol: Request.Url.Scheme);
+            var linkDeCallback =
+                Url.Action(
+                    "ConfirmacaoEmail",
+                    "Conta",
+                    new { usuarioId = usuario.Id, token = token },
+                    Request.Url.Scheme);
 
             await UserManager.SendEmailAsync(
                 usuario.Id,
-                "Confirmação de registro",
-                $"Bem vindo ao forum ByteBank. Para confirmar seu email clique aqui {callbackUrl}");
+                "Fórum ByteBank - Confirmação de Email",
+                $"Bem vindo ao fórum ByteBank, clique aqui {linkDeCallback} para confirmar seu email!");
         }
 
-        public ActionResult ConfirmacaoEmail(string usuarioId, string codigo)
+        public ActionResult ConfirmacaoEmail(string usuarioId, string token)
         {
             throw new NotImplementedException();
         }
-
+        
         private void AdicionaErros(IdentityResult resultado)
         {
             foreach (var erro in resultado.Errors)
